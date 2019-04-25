45f05464d3bab32cc7bde514b1e59b894b4c0467
Erick Carvajal R <neocarvajal@gmail.com>
2019-04-25 10:28:31 -0400

    all project

---
+++ presearch.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python -*- coding: utf-8 -*-
+
+"""
+Python script for automated  firefox web browser search on presearch.org.
+
+Presearch is an open, decentralized search engine that rewards community members 
+with Presearch Tokens for their usage, contribution to, and promotion of the platform.
+
+All this project is based on Selenium. You can install it from here:
+https://selenium-python.readthedocs.io/installation.html
+
+web: https://www.presearch.org/signup?rid=1257758
+"""
+
+import time
+import os
+import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+HOME_DIRECTORY = os.environ.get('HOME')
+
+options = webdriver.FirefoxOptions()
+
+options.add_argument("--width=800");
+
+options.add_argument("--height=640");
+
+profile = webdriver.FirefoxProfile(HOME_DIRECTORY + "/.mozilla/firefox/fao18bjc.default")
+
+options.binary_location = "/usr/bin/firefox"
+
+browser = webdriver.Firefox(firefox_profile=profile,firefox_options=options)
+
+def busqueda(palabra):		
+	search = browser.find_element_by_id('search')
+	search.send_keys(palabra)
+	print("Buscando... " + palabra)
+	submit = browser.find_element_by_xpath("//button[@type='submit']")
+	submit.click()
+	time.sleep(10)
+	try:
+	    element = WebDriverWait(browser, 10).until(
+	        EC.presence_of_element_located((By.ID, "logo"))
+	    )
+	finally:		
+		browser.implicitly_wait(10)		
+		print("Navegando a presearch...")
+		browser.get('https://presearch.org/')
+
+print("Navegando a presearch...")
+
+browser.get('https://presearch.org/')
+
+try:
+    element = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.ID, "Layer_1"))
+    )
+finally:
+
+	archivo = open("search.txt", "r")	
+
+	for palabra in archivo.readlines():	    
+		busqueda(palabra)
+
+	browser.close()
+
+__author__ = "Erick Carvajal"
+__copyright__ = "Copyright 2019, presearch.py"
+__license__ = "MIT"
+__version__ = "1.0"
+__maintainer__ = "@neocarvajal"
+__email__ = "neocarvajal@gmail.com"
+__status__ = "Production"
